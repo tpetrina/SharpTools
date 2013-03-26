4d1c562da6c3dd3f446a824f25e6a7fff3769edf
Toni Petrina <toni.petrina@gmail.com>
2013-03-26 16:44:06 +0100

    Adding basic editor page and simple response service

---
+++ RoslynPerspective/Controllers/CompilerController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using System.Web.Http;
+using Roslyn.Compilers.CSharp;
+
+namespace RoslynPerspective.Controllers
+{
+    public class CompilerStatus
+    {
+        public string Status { get; set; }
+    }
+
+    public class CompilerController : ApiController
+    {
+        public string Post([FromBody] string data)
+        {
+            if (data == null)
+                return "Must send some data";
+
+            try
+            {
+                var tree = SyntaxTree.ParseText(data);
+                var visitor = new Visitor();
+                visitor.Visit(tree.GetRoot());
+                return visitor.Tree;
+            }
+            catch (Exception e)
+            {
+                return string.Format("Exception caught: '{0}'", e);
+            }
+        }
+    }
+
+    internal class Visitor : SyntaxRewriter
+    {
+        private int _indent;
+        private readonly StringBuilder _sb = new StringBuilder();
+
+        public string Tree
+        {
+            get { return _sb.ToString(); }
+        }
+
+        public override SyntaxNode Visit(SyntaxNode node)
+        {
+            if (node == null)
+                return base.Visit(null);
+
+            _sb.AppendLine(new string(' ', _indent * 2) + node.Kind.ToString());
+            _indent++;
+
+            var node2 = base.Visit(node);
+            _indent--;
+            return node2;
+        }
+    }
+}
---
+++ RoslynPerspective/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace RoslynPerspective.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
